e28331789c28b83971090250cbf0a8f6debc2153
Pirisinu <palazzialessandro2@gmail.com>
2023-10-06 15:39:25 +0200

    [ADD]Random email in Html

--- js/script.js
+++ js/script.js
@@ -4,11 +4,24 @@ createApp({
 
   data(){
     return{
-
+      emails: [],
+      apiUrl: 'https://ﬂynn.boolean.careers/exercises/api/random/mail',
+      emailRequest: 10
+    }
+  },
+  methods:{
+    getApi(){
+      if(this.emails.length >= this.emailRequest) return
+      axios.get(this.apiUrl)
+        .then( (response) => {
+          this.emails.push(response.data.response)
+          this.getApi()
+        } )
     }
   },
   mounted(){
-    console.log('ciao da Vue');
+    this.getApi()
+    console.log(this.emails);
   }
 
 }).mount('#app')
